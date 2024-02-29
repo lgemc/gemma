8f9d82c065343d9bf75002fb88bd79c4fc91e4e4
Luis Manrique Cardona <luis.manrique.car@gmail.com>
2024-02-29 02:24:03 +0000

    (feat): init gemma 7b-it cuda

---
+++ main.py
@@ -0,0 +1,12 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM,BitsAndBytesConfig
+auth_token = "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"
+
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it", token=auth_token)
+model = AutoModelForCausalLM.from_pretrained("google/gemma-7b-it", device_map="auto", token=auth_token)
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
